25a262a5e650dad397e0edb262eb5cee403cfeb3
Maxim <fixor@yandex.ru>
2025-08-15 13:46:48 +0300

    sprint 5

--- src/main.js
+++ src/main.js
@@ -36,12 +36,17 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-  if (!data || !Array.isArray(data.sellers) || data.sellers.length === 0) {
+  if (!data || !Array.isArray(data.sellers) || data.sellers.length === 0
+    || !Array.isArray(data.products) || data.products.length === 0
+    || !Array.isArray(data.purchase_records) || data.purchase_records.length === 0
+  ) {
     throw new Error('Некорректные входные данные');
   }
   const {calculateRevenue, calculateBonus} = options;
-  if (!typeof calculateRevenue === "function" || !typeof calculateBonus === "function") {
-    throw new Error('Чего-то не хватает');
+  if (!typeof options === "object" || !typeof calculateRevenue === "function"
+    || !typeof calculateBonus === "function"
+  ) {
+    throw new Error('Неверные параметры');
   }
   const sellerStats = data.sellers.map(seller => ({
     id: seller.id,
